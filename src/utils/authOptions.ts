b6074dca2a61106942b4ecfe0178cf76e0db2cb3
subas-roy <subasroy46@gmail.com>
2025-05-02 11:30:11 +0600

    Added GitHubProvider to authOptions' providers

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -1,8 +1,13 @@
 import { NextAuthOptions } from 'next-auth';
+import GitHubProvider from 'next-auth/providers/github';
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     // add providers here
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
   ],
 };
