1b9aaffab9517b2c5b1f5986f444920bcf300e01
subas-roy <subasroy46@gmail.com>
2025-05-02 11:57:20 +0600

    On clicking, it redirects to the github account

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -105,7 +105,7 @@ const LoginPage = () => {
             </button>
             <button
               className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shadow-md hover:bg-gray-200"
-              onClick={() => signIn()}
+              onClick={() => signIn('github')}
             >
               <Image
                 src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
