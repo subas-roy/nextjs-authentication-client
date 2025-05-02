a6a6f1ef9c9fc2ab208e1d1bc6764d028cd7c5a4
subas-roy <subasroy46@gmail.com>
2025-05-02 17:51:03 +0600

    Added onclidk() event handler to Google Sign In button

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -95,7 +95,14 @@ const LoginPage = () => {
 
           {/* Social Login Buttons */}
           <div className="flex justify-center gap-4 mt-4">
-            <button className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shadow-md hover:bg-gray-200">
+            <button
+              className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-full shadow-md hover:bg-gray-200"
+              onClick={() =>
+                signIn('google', {
+                  callbackUrl: 'http://localhost:3000/dashboard',
+                })
+              }
+            >
               <Image
                 src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
                 width={30}
