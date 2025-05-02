7c1918eb5fe0d0c58042513d3f3ce8b9cdf90e8e
subas-roy <subasroy46@gmail.com>
2025-05-02 17:34:40 +0600

    Implemented user logout functionality

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { signOut } from 'next-auth/react';
 import Link from 'next/link';
 
 type UserProps = {
@@ -75,7 +77,10 @@ const Navbar = ({ session }: { session: UserProps | null }) => {
 
       <div className="flex items-center">
         {session?.user ? (
-          <button className="border border-red-500 text-red-500 px-5 py-2 rounded-full hover:bg-red-500 hover:text-black transition duration-200">
+          <button
+            className="border border-red-500 text-red-500 px-5 py-2 rounded-full hover:bg-red-500 hover:text-black transition duration-200"
+            onClick={() => signOut()}
+          >
             Logout
           </button>
         ) : (
