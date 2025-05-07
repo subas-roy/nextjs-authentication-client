28dfde4265f6f197fa91c04fde75975c56ad37dc
subas-roy <subasroy46@gmail.com>
2025-05-07 13:10:21 +0600

    Public '/' or home route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/dashboard', '/'], // Add any other paths you want to protect with authentication
+  matcher: ['/dashboard'],
   // matcher: ['/dashboard/:path*', '/profile/:path*'], // Example for matching all paths under /dashboard and /profile
 };
