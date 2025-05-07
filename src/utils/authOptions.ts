3f95e9eb60e06189da7e0b1024ce4400d044acf0
subas-roy <subasroy46@gmail.com>
2025-05-07 12:08:12 +0600

    Redirect user to login page after logging out, and if unauthorized user want to visit private route

--- src/utils/authOptions.ts
+++ src/utils/authOptions.ts
@@ -15,5 +15,8 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_SECRET as string,
     }),
   ],
+  pages: {
+    signIn: '/login',
+  },
   secret: process.env.NEXTAUTH_SECRET,
 };
