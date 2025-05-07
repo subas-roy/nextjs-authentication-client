d99cbef63540e206e458a6013396fb0e9abd0444
subas-roy <subasroy46@gmail.com>
2025-05-07 13:00:44 +0600

    Protect all child route under specific route, and add more routes to protect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,6 @@
 export { default } from 'next-auth/middleware';
 
-export const config = { matcher: ['/dashboard'] };
+export const config = {
+  matcher: ['/dashboard', '/'], // Add any other paths you want to protect with authentication
+  // matcher: ['/dashboard/:path*', '/profile/:path*'], // Example for matching all paths under /dashboard and /profile
+};
