47acc5d5e51cd8a334775701a550a78a19d40cf6
subas-roy <subasroy46@gmail.com>
2025-05-07 17:57:07 +0600

    Added default user image

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -15,9 +15,13 @@ const DashboardPage = async () => {
           </h1>
           <h1 className="text-3xl text-center mt-10">{session?.user?.email}</h1>
           <Image
-            src={session?.user?.image}
+            src={
+              session?.user?.image ||
+              'https://cdn.pixabay.com/photo/2013/07/12/14/36/man-148582_1280.png'
+            }
             width={100}
             height={100}
+            alt="user image"
             className="mx-auto rounded-full mt-10"
           />
         </>
